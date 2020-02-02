38e5d9f6007b365073881f5476ca7cb056156299
HristoP96 <HPopov@infragistics.com>
2020-02-02 19:23:02 +0200

    Update terrain

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    int playTime = 60;
+    int seconds;
+    int minutes;
+
+    int winTreshold = 1;
+	// Use this for initialization
+	void Start () {
+
+
+        StartCoroutine("PlayTimer");
+	}
+	
+    IEnumerator PlayTimer()
+    {
+        while (playTime > 0)
+        {
+            yield return new WaitForSeconds(1);
+            playTime--;
+            seconds = playTime % 60;
+            minutes = playTime / 60 % 60;
+
+            UIManager.instance.UpdateTime(minutes, seconds);
+        }
+
+        if (hasWon())
+        {
+            UIManager.instance.UpdateResult("Congratulations", Color.green);
+        } else
+        {
+            UIManager.instance.UpdateResult("You lose", Color.red);
+        }
+
+        UIManager.instance.CleanCanvas();
+
+    }
+
+    bool hasWon()
+    {
+        return ScoreManager.ReadScore() >= winTreshold;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,17 +6,38 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour {
 
     public static UIManager instance;
-
+    public Text scoreText;
+    public Text timeText;
+    public Text resultText;
     public void Awake()
     {
         instance = this;
         UpdateUI();
+        UpdateTime(1, 0);
+        resultText.text = "";
     }
 
-    public Text scoreText;
-    
+
+
+    public void UpdateTime(int min, int seconds)
+    {
+        timeText.text = min.ToString("D2") + ":" + seconds.ToString("D2");
+    }
     public  void UpdateUI()
     {
         scoreText.text = "Score: " + ScoreManager.ReadScore();
+    }
+
+    public void UpdateResult(string result, Color color)
+    {
+        resultText.text = result;
+        resultText.color = color;
+    }
+
+    public void CleanCanvas()
+    {
+        scoreText.text = "";
+        timeText.text = "";
+
     }
 }
