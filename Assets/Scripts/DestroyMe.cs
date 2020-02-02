1da615f9c1b65f4c5bc273951440ee484d3e240b
HristoP96 <HPopov@infragistics.com>
2020-02-02 15:11:06 +0200

    adding events

---
+++ Assets/Scripts/DestroyMe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMe : MonoBehaviour {
+
+    Collider col;
+
+    private void Start(){
+        col = GetComponent<Collider>();
+
+        col.enabled = false;
+    }
+
+    public void DestroyObj() {
+        Destroy(gameObject);
+    }
+
+    public void SwitchCollider(int num) {
+        col.enabled = (num == 0) ? false : true;
+    }
+}
